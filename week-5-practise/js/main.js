9a65189e07fd7a99a4f0eae11979dba18f4e0d0b
Soha <s.azadi12319@gmail.com>
2018-03-31 11:30:13 +0100

    week5

---
+++ week-5-practise/js/main.js
@@ -0,0 +1,51 @@
+var blueBtn = document.querySelector("#blueBtn");
+blueBtn.addEventListener("click", function() {
+  var box = document.querySelector(".jumbotron");
+  box.style.backgroundColor = "#588fbd";
+});
+
+blueBtn.addEventListener("click", function() {
+  var donateBtn = document.querySelector("#donateBtn");
+  donateBtn.style.backgroundColor = "#ffa500";
+});
+
+blueBtn.addEventListener("click", function() {
+  var volunteerBtn = document.querySelector("#volunteerBtn");
+  volunteerBtn.style.backgroundColor = "black";
+});
+
+/*orangeBtn*/
+var orangeBtn = document.querySelector("#orangeBtn");
+orangeBtn.addEventListener("click", function() {
+  var box = document.querySelector(".jumbotron");
+  box.style.backgroundColor = `#f0ad4e`;
+});
+
+orangeBtn.addEventListener("click", function() {
+  var donateBtn = document.querySelector("#donateBtn");
+  donateBtn.style.backgroundColor = "#5751fd";
+});
+
+orangeBtn.addEventListener("click", function() {
+  var volunteerBtn = document.querySelector("#volunteerBtn");
+  volunteerBtn.style.backgroundColor = "#31b0d5";
+});
+
+/*greenBtn*/
+
+var greenBtn = document.querySelector("#greenBtn");
+greenBtn.addEventListener("click", function() {
+  var box = document.querySelector(".jumbotron");
+  box.style.backgroundColor = `#87ca8a`;
+});
+
+greenBtn.addEventListener("click", function() {
+  var donateBtn = document.querySelector("#donateBtn");
+  donateBtn.style.backgroundColor = "black";
+});
+
+greenBtn.addEventListener("click", function() {
+  var volunteerBtn = document.querySelector("#volunteerBtn");
+  volunteerBtn.style.backgroundColor = `#8c9c08`;
+  volunteerBtn.style.color = "white";
+});
